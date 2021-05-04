15094a726b401409b6b8a30a5205cc3ae60267f9
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-05-04 17:40:24 +0100

    adding-markup continued

--- wp-content/themes/wphierarchy/index.php
+++ wp-content/themes/wphierarchy/index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-    <h1>index.php</h1>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h1>index.php</h1>
+                </header>
+                <div class="enyrt-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit inventore reiciendis neque aspernatur eos tempora ipsa expedita rerum, aperiam ab! Harum officia adipisci est a. Eveniet, rerum? Mollitia, quo deserunt?</p>
+                </div>
+            </article>
+        </main>
+    </div>
 <?php get_footer(); ?>
