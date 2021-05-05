0218fa904e231b567fb0bee36ab44d99beb9fbab
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-05-05 18:07:47 +0100

    updated markup

--- wp-content/themes/wphierarchy/header-splash.php
+++ wp-content/themes/wphierarchy/header-splash.php
@@ -1,7 +1,29 @@
-<?php get_header(); ?>
-    <h1>index.php</h1>
-</body>
-<div class="notice">
-    <p>NEW - Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui perspiciatis cupiditate totam molestias! Quidem harum, fugit, pariatur dolore fugiat error expedita modi eligendi molestiae quam explicabo commodi quae, tempore voluptatum.</p>
-</div>
-</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_heade(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="page">
+        <a href="#content" class="skip-link screen-reader-text">
+            <?php esc_html_e('Skip to content', 'wphierarchy'); ?>
+        </a>
+        <header id="masthead" class="site-header" role="banner">
+            <div class="notice">
+                <p>NEW - Lorem to sell thissum!</p>
+            </div>
+            <div class="site-branding">
+                <p class="site-title">
+                    <?php bloginfo('name'); ?>
+                </p>
+                <p class="site-description">
+                    <?php bloginfo('description') ?>
+                </p>
+            </div>
+        </header>
+        <div id="content" class="site-content">
+        
