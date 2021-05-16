6ba14518f8e515c82a46ffd24e57fb5e6e17a074
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-05-16 03:32:25 +0100

    added content-gallery.php, learned how single blog posts are displayed

--- wp-content/themes/wphierarchy/template-parts/content-gallery.php
+++ wp-content/themes/wphierarchy/template-parts/content-gallery.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
         <span class="dashicons dashicons-format-<?php echo get_post_format($post->ID); ?>"></span>
-        <p><?php esc_html_e('Enjoy this gallery post'); ?></p>
+        <p><?php esc_html_e('Enjoy this gallery post', 'wphierarchy'); ?></p>
         <?php the_title('<h1>', '</h1>'); ?>
     </header>
         <div class="enyrt-content">
