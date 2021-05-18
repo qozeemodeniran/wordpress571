cb4182968429b2f1e533ca72eefef2e314ed6230
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-05-18 06:36:03 +0100

    archive.php

---
+++ wp-content/themes/wphierarchy/archive.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+    <div id="primary" class="content-area">
+        <?php get_sidebar(); ?>
+        <main id="main" class="site-main" role="main">
+            <h1><?php wp_title('-posts'); ?></h1>
+            <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                <?php get_template_part('template-parts/content-posts', get_post_format( )); ?>
+            <?php endwhile; else: ?>
+                <?php get_template_part('template-parts/content', 'none'); ?>
+            <?php endif; ?>
+            <?php echo paginate_links(); ?>
+            <p>Template: archive.php</p>
+        </main>
+    </div>
+<?php get_footer(); ?>
