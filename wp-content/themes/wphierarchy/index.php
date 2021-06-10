d9811db16f458297c91b6117cde3b04d30133da6
Qozeem Odeniran <Qozeem>
2021-06-10 12:49:55 +0100

    archive tags part-1

--- wp-content/themes/wphierarchy/index.php
+++ wp-content/themes/wphierarchy/index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
     <div id="primary" class="content-area">
-        <?php get_sidebar(); ?>
+        <h1><?php single_post_title('Post: '); ?></h1>
+        <h2><?php single_cat_title('Category: '); ?></h2>
         <main id="main" class="site-main" role="main">
             <?php if(have_posts()): while(have_posts()): the_post(); ?>
                 <?php get_template_part('template-parts/content', get_post_format( )); ?>
@@ -10,4 +11,5 @@
             <p>Template: index.php</p>
         </main>
     </div>
+    <?php get_sidebar(); ?>
 <?php get_footer(); ?>
